6aa3931b4c70821025fb8fa31f36e814f3754d9c
deepaksharma26 <ds75279@gmail.com>
2023-10-01 23:28:20 +0530

    Jenkins File

--- src/App.js
+++ src/App.js
@@ -2,10 +2,15 @@ import logo from './logo.svg';
 import './App.css';
 import { AppRoutes } from './routes/Routes'; 
 import { BrowserRouter } from "react-router-dom";
+import { Header } from './common/Header';
+import { Footer } from './common/Footer';
 function App() {
   return ( 
+    
       <BrowserRouter>
+      <Header/>
         <AppRoutes/>
+        <Footer/>
       </BrowserRouter> 
   );
 }
